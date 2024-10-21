9bb3e336ee4162633944c510311246fb85e85831
WaiHlyan <waihlyan.23@ichat.sp.edu.sg>
2024-10-21 10:32:34 +0800

    added sort_temp, median

--- bmi.py
+++ bmi.py
@@ -20,7 +20,9 @@ def display_main_menu():
     print("Enter some numbers separated by commas (e.g. 5, 67, 32):")
     user_input = get_user_input()  
     calc_average_temperature(user_input) 
-
+    find_min_max(user_input)
+    sorted_list = sort_temperature(user_input)
+    calc_median_temperature(sorted_list)
 
 def get_user_input():
     user_input = input()  
@@ -37,6 +39,30 @@ def calc_average_temperature(float_list):
     average_temp = total / len(float_list)
     print(f"The average temperature is: {average_temp}")
     
+
+def find_min_max(float_list):
+    min_max = []
+    Min = min(float_list)
+    min_max.append(Min)
+    Max = max(float_list)
+    min_max.append(Max)
+    print("Minimum and maximin numbers are: " , min_max)
+   
+    
+def sort_temperature(float_list):
+    sorted_list = sorted(float_list)
+    print("The list in ascending order: ", sorted_list)
+    return sorted_list
+    
+def calc_median_temperature(float_list):
+
+    length = len(float_list)
+    mid = length // 2
+    
+    if length % 2 == 1:
+        print("The median is: ", float_list[mid])
+    else:
+        print("The median is: ", (float_list[mid - 1] + float_list[mid]) / 2)
 
 
 
