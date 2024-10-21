a415adec0a276a5a2bc9b1c2e82beb4039e173a0
WaiHlyan <waihlyan.23@ichat.sp.edu.sg>
2024-10-21 09:53:35 +0800

    finish average temperature

--- bmi.py
+++ bmi.py
@@ -1,14 +1,44 @@
 def calculate_bmi(height, weight):
-    print("Height = " + str(height) )
-    print("Weight = " + str(weight) )
-    bmi = weight/(height*height)
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+    bmi = weight / (height * height)
     if bmi < 18.5:
         print("UnderWeight")
     elif 18.5 <= bmi <= 25.0:
-        print("Normal Wight")
+        print("Normal Weight")
     elif bmi > 25.0:
         print("OverWeight")
-        
-    print("Your Bmi value is = " + str(bmi))
     
-calculate_bmi(weight=57, height=1.73)
+    print("Your Bmi value is = " + str(bmi))
+
+
+def main():
+    display_main_menu()
+
+
+def display_main_menu():
+    print("Enter some numbers separated by commas (e.g. 5, 67, 32):")
+    user_input = get_user_input()  
+    calc_average_temperature(user_input) 
+
+
+def get_user_input():
+    user_input = input()  
+    formatted_input = user_input.split(",")
+    float_list = [float(item) for item in formatted_input]
+    print("Converted to float list:", float_list)
+    return float_list 
+
+
+def calc_average_temperature(float_list):
+    total = 0  
+    for i in float_list:
+        total += i
+    average_temp = total / len(float_list)
+    print(f"The average temperature is: {average_temp}")
+    
+
+
+
+if __name__ == "__main__":
+    main()
